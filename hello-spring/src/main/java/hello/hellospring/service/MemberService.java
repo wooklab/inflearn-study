a59765f964f3cbb68c70b3fe76786f4234285e8f
Hwang In-wook <inwook9@gmail.com>
2021-01-03 01:20:57 +0900

    스프링 데이터 JPA

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -3,9 +3,12 @@ package hello.hellospring.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 
+@Transactional  // JPA를 통한 모든 데이터 변경은 트랜잭션 안에서 실행해야 한다.
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -17,6 +20,7 @@ public class MemberService {
     /**
      * 회원 가입
      */
+    // @Transactional  // 현재 해당 서비스에서 데이터 변경작업은 join() 박에 없기 때문에 여기에만 추가해도 된다.
     public Long join(Member member) {
         // 같은 이름이 있는 중복 회원X
         validateDuplicateMember(member); // 중복회원검증
