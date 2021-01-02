0a024bc0bc2f9a6f5e792fbada0739238344e29f
Hwang In-wook <inwook9@gmail.com>
2021-01-02 18:39:20 +0900

    스프링 빈과 의존관계

---
+++ hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
@@ -0,0 +1,20 @@
+package hello.hellospring.service;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
