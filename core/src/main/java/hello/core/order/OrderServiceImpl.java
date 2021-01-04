b36160ed0cd021f67bed0363174edd04d7de6624
Hwang In-wook <inwook9@gmail.com>
2021-01-05 00:10:53 +0900

    의존관계 자동 주입 - 옵션처리

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -13,41 +13,12 @@ import hello.core.member.MemoryMemberRepository;
 @Component
 public class OrderServiceImpl implements OrderService {
 
-    // private final MemberRepository memberRepository = new MemoryMemberRepository();
-    // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    // private final DiscountPolicy discountPolicy = new RateDiscountPolicy();  // 추상인터페이스 뿐만아니라 구현클래스에도 의존
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
 
-    /*private MemberRepository memberRepository;
-    private DiscountPolicy discountPolicy;
-
-    @Autowired
-    public void setMemberRepository(MemberRepository memberRepository) {
-        System.out.println("memberRepository = " + memberRepository);
-        this.memberRepository = memberRepository;
-    }
-
-    @Autowired
-    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
-        System.out.println("discountPolicy = " + discountPolicy);
-        this.discountPolicy = discountPolicy;
-    }
-
-    @Autowired  // 생성자가 하나일 경우 생략이 가능
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         System.out.println("OrderServiceImpl.OrderServiceImpl");
         System.out.println("memberRepository = " + memberRepository + ", discountPolicy = " + discountPolicy);
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }*/
-
-    /*@Autowired private MemberRepository memberRepository;
-    @Autowired private DiscountPolicy discountPolicy;*/
-
-    private MemberRepository memberRepository;
-    private DiscountPolicy discountPolicy;
-
-    @Autowired
-    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
