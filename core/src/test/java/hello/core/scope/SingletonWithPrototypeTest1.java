5d0df56e8f77dacd693229cf55f50887685e927f
Hwang In-wook <inwook9@gmail.com>
2021-01-07 00:32:23 +0900

    프로토타입 스코프 - 싱글톤 빈과 함께 사용시 문제점 - 프로토타입 스코프 - 싱글톤 빈과 함께 사용시   Provider 로 문제점해결

--- core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -4,8 +4,12 @@ import static org.assertj.core.api.Assertions.*;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -34,20 +38,22 @@ class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
 
         ac.close();
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean;      // 생성시점 주입
+        @Autowired
+        private Provider<PrototypeBean> prototypeBeanProvider;
+        // private ObjectProvider<PrototypeBean> prototypeBeanObjectProvider;  // ObjectFactory 확장
+        // private ObjectFactory<PrototypeBean> prototypeBeanObjectProvider; // 예전에 사용
 
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
 
         public int logic() {
+            // PrototypeBean prototypeBean = prototypeBeanObjectProvider.getObject();  // DL(Dependency Lookup)
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();  // DL(Dependency Lookup)
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
