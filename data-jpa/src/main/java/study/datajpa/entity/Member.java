b78f0778213fe9a310b002d104d31ccf39328592
Hwang In-wook <inwook9@gmail.com>
2021-03-11 00:47:36 +0900

    쿼리 메소드 기능: JPA NamedQuery

--- data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -1,12 +1,6 @@
 package study.datajpa.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -15,12 +9,18 @@ import lombok.Setter;
 import lombok.ToString;
 
 @Entity
-@Getter @Setter
+@Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED) // private 하면 안됨.
 @ToString(of = {"id", "username", "age"})   // team 은 제외. 서로 참조하여 무한 loop가 될 수 있음 (연관관계는 toString에서 제외하자)
+@NamedQuery(    // 실무에서는 거의 안쓴는 NamedQuery, 하지만 애플리케이션 로드 시점에 쿼리 오류를 확인할 수 있어 유용한 기능도 있다.
+        name = "Member.findByUsername",
+        query = "select m from Member m where m.username = :username"
+)
 public class Member {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     @Column(name = "member_id")
     private Long id;
     private String username;
--- data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,6 +1,8 @@
 package study.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import study.datajpa.entity.Member;
 
 import java.util.List;
@@ -8,4 +10,12 @@ import java.util.List;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    /*
+     * 먼저는 NamedQuery 를 찾고
+     * 이 후에 method name 으로 유추해낸 쿼리를 생성한다.
+     */
+//    @Query(name = "Member.findByUsername")  // SpringJPA 에서 해당 어노테이션은 생략가능
+    List<Member> findByUsername(@Param("username") String username);
+
 }
