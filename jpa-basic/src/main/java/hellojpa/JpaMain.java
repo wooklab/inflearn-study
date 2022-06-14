9e3365427496f16edd1bf4b371c0618395117cc7
In-wook Hwang <inwook9@gmail.com>
2022-06-15 00:55:09 +0900

    jap basic - JPQL

--- jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -16,9 +17,14 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = em.find(Member.class, 1L);
-            member.setName("HelloJPA");
+            List<Member> resultList = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(5)  // 페이징 처리
+                    .setMaxResults(10)
+                    .getResultList();
 
+            for (Member member : resultList) {
+                System.out.println("member.name = " + member.getName());
+            }
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
