75b9bc00204bdac623cb5037a017d4346ca085d5
In-wook Hwang <inwook9@gmail.com>
2022-06-16 00:19:59 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편 영속성 관리 - 내부동작방식 - 영속성 컨텍스트 2-1

--- jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -18,13 +18,18 @@ public class JpaMain {
         try {
             // 비영속
             Member member = new Member();
-            member.setId(100L);
+            member.setId(101L);
             member.setName("HelloJPA");
 
             // 영속
             System.out.println("=== BEFORE ===");
             em.persist(member);
             System.out.println("=== AFTER ===");
+
+            // commit 전 조회를 했지만 결과가 반환됨(1차 캐시에 데이터가 있기 때문)
+            Member findMember = em.find(Member.class, 101L);
+            System.out.println("findMember.id = " + findMember.getId());
+            System.out.println("findMember.name = " + findMember.getName());
 
             tx.commit();    // 실제 쿼리가 발생하는 시점
         } catch (Exception e) {
