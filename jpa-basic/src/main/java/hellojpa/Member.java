241b12fa521edaf01bb2b82174bfb6fe35266780
Hwang In-wook <inwook9@gmail.com>
2022-06-17 00:36:51 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편 엔티티 매핑 - 객체와 테이블 매핑

--- jpa-basic/src/main/java/hellojpa/Member.java
+++ jpa-basic/src/main/java/hellojpa/Member.java
@@ -2,15 +2,17 @@ package hellojpa;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
-@Entity
+@Entity // default (name = "Member")// JPA 가 내부적으로 구분하기 위한 이름
+@Table // (name = "MBR") // 추약어 사용시 이름을 명시하여 클래스명을 다르게 작성할 수 있다
 public class Member {
 
     @Id
     private Long id;
     private String name;
 
-    // JPA는 내부적으로 리플렉션 사용함으로 기본 생성자 필요
+    // JPA는 내부적으로 리플렉션 사용함으로 기본 생성자 필요(public이나 protected)
     public Member() {
     }
 
