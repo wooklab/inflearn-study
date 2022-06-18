12f9cc09ef4994886fc1f87e4c8753f64ab645ef
Hwang In-wook <inwook9@gmail.com>
2022-06-18 23:52:32 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편 엔티티 매핑 - 기본 키 매핑3 (SequenceGenerator)

--- jpa-basic/src/main/java/hellojpa/Member.java
+++ jpa-basic/src/main/java/hellojpa/Member.java
@@ -5,14 +5,19 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity // default (name = "Member")// JPA 가 내부적으로 구분하기 위한 이름
 @Table // (name = "MBR") // 추약어 사용시 이름을 명시하여 클래스명을 다르게 작성할 수 있다
+@SequenceGenerator(
+    name = "member_seq_generator",
+    sequenceName = "member_seq"
+)
 public class Member {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_generator")
     private Long id;
 
     // columnDefinition = "varchar(255)"
