b3a1e99874d7a5061f9c815cfa9a0170147402f9
In-wook Hwang <inwook9@gmail.com>
2022-06-19 23:21:45 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편 연관관계 매핑 기초 - 양방향 연관관계와 연관관계의 주인 2   (연관관계 주인과 주인이 아닌쪽에 모두 값을 넣어주자)

--- jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -26,15 +26,19 @@ public class JpaMain {
             member.setTeam(team);
             em.persist(member);
 
-            em.flush();
-            em.clear();
+//            team.getMembers().add(member);  // 객체지향적으로 넣어주는게 맞음
+
+//            em.flush();
+//            em.clear();   // 일부러 flush() 하지 않는다면 같은 트랜잭션 상에서는 JPA 관계가 적용되기 전이다.
 
             Member findMember = em.find(Member.class, member.getId());
             List<Member> members = findMember.getTeam().getMembers();
 
+            System.out.println("===================");
             for (Member m : members) {
                 System.out.println("m.username = " + m.getUsername());
             }
+            System.out.println("===================");
 
             tx.commit();
         } catch (Exception e) {
