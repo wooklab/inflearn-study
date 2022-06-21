ab109869ebfc5f58a00eb239d559083ad7d0d922
Hwang In-wook <inwook9@gmail.com>
2022-06-21 22:32:33 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편 다양한 연관관계 매핑 - 일대다 (1:N)에서 양방향(읽기전용) 추가하기

--- jpa-basic/src/main/java/hellojpa/Member.java
+++ jpa-basic/src/main/java/hellojpa/Member.java
@@ -4,6 +4,8 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity // default (name = "Member")// JPA 가 내부적으로 구분하기 위한 이름
 public class Member {
@@ -16,6 +18,10 @@ public class Member {
     // columnDefinition = "varchar(255)"
     @Column(name = "USERNAME") // DDL 생성기능으로 자동 스키마 생성시에만 작동
     private String username;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false)    // 일대다 에서 양방향(읽기전용)을 사용하고 싶은 경우 추가
+    private Team team;
 
     protected Member() {
         // do nothing just for hibernate...
