8a944373f1ea4dcc3f16294f7bdb5a98229d0ed1
Hwang In-wook <inwook9@gmail.com>
2022-06-23 00:39:10 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편 고급매핑 매핑 - 실전예제 4 - 상속관계 매핑 (JOINED)

--- jpa-basic/src/main/java/jpabook/jpashop/domain/Item.java
+++ jpa-basic/src/main/java/jpabook/jpashop/domain/Item.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@Inheritance(strategy = InheritanceType.JOINED)
 @DiscriminatorColumn
 public abstract class Item {
 
