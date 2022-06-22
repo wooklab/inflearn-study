7595c6917f85da85ea1fe6b2d418bd6ae1ce7abf
Hwang In-wook <inwook9@gmail.com>
2022-06-22 23:56:06 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편 고급매핑 매핑 - 상속관계 매핑: JOIN 전략 (DTYPE 추가)

--- jpa-basic/src/main/java/hellojpa/NewItem.java
+++ jpa-basic/src/main/java/hellojpa/NewItem.java
@@ -1,5 +1,6 @@
 package hellojpa;
 
+import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -8,6 +9,7 @@ import javax.persistence.InheritanceType;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn    // DTYPE이 자동추가
 public class NewItem {
 
     @Id
