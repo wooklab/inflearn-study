74ea0870fb28410d5a557b267f2086c0f881689f
Hwang In-wook <inwook9@gmail.com>
2022-06-24 22:58:06 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편 프록시와 연관관계 관리 - 프록시 (proxy 조회)

--- jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -25,9 +25,12 @@ public class JpaMain {
             em.clear();
 
             // join 쿼리로 member와 team을 찾음
-            Member findMember = em.find(Member.class, member.getId());
-            System.out.println("findMember.username = " + findMember.getUsername());
+//            Member findMember = em.find(Member.class, member.getId());
+            Member findMember = em.getReference(Member.class, member.getId());  // 해당 단계에선 아무것도 출력되지 않음
+            System.out.println("findMember = " + findMember.getClass());    // findMember = class hellojpa.Member$HibernateProxy$Ndn8OAFg
             System.out.println("findMember.id = " + findMember.getId());
+            System.out.println("findMember.username = " + findMember.getUsername());    // 해당 단계에서 DB에 쿼리
+            System.out.println("findMember.username = " + findMember.getUsername());    // 이미 초기화 후에는 쿼리 불필요
 
             tx.commit();
         } catch (Exception e) {
