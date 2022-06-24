7fe7669b3a39c1b783162559e5f3b539c2257365
Hwang In-wook <inwook9@gmail.com>
2022-06-25 00:57:57 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편 프록시와 연관관계 관리 - 프록시 (영속성 컨텍스트에 이미 있으면 프록시를 호출해도 원본)

--- jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -20,17 +20,16 @@ public class JpaMain {
             member1.setUsername("member1");
             em.persist(member1);
 
-            Member member2 = new Member();
-            member2.setUsername("member2");
-            em.persist(member2);
-
             em.flush();
             em.clear();
 
             Member m1 = em.find(Member.class, member1.getId());
-            Member m2 = em.getReference(Member.class, member2.getId());
+            System.out.println("m1 = " + m1.getClass());    // 영속성 컨텍스트에 저장됨
 
-            logic(m1, m2);
+            Member reference = em.getReference(Member.class, member1.getId());
+            System.out.println("reference = " + reference.getClass()); // 원본을 가리킴
+
+            System.out.println("a == a: " + (m1 == reference)); // JPA 에서는 한 트랜잭션안에서는 같은 걸 보장해준다.
 
             tx.commit();
         } catch (Exception e) {
@@ -41,8 +40,4 @@ public class JpaMain {
         emf.close();
     }
 
-    private static void logic(Member m1, Member m2) {
-        System.out.println("m1 == m2: " + (m1 instanceof Member));
-        System.out.println("m1 == m2: " + (m2 instanceof Member));
-    }
 }
