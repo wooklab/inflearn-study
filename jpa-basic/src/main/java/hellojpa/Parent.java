43857969e8cd544acff29828f8c641be7d5c8d41
Hwang In-wook <inwook9@gmail.com>
2022-06-27 00:07:29 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편 프록시와 연관관계 관리 - 고아객체

--- jpa-basic/src/main/java/hellojpa/Parent.java
+++ jpa-basic/src/main/java/hellojpa/Parent.java
@@ -17,7 +17,7 @@ public class Parent {
 
     private String name;
 
-    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Child> childList = new ArrayList<>();
 
     public void addChild(Child child) {
@@ -39,5 +39,13 @@ public class Parent {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Child> getChildList() {
+        return childList;
+    }
+
+    public void setChildList(List<Child> childList) {
+        this.childList = childList;
     }
 }
