aff33bde3d7559611a9b9ee9b5f12f1fa234413f
Hwang In-wook <inwook9@gmail.com>
2022-06-29 23:20:11 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편 객체지향 쿼리 언어1 - 기본문법: 소개 - criteria 쿼리 기본 작성

--- jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 public class JpaMain {
@@ -17,11 +20,16 @@ public class JpaMain {
         tx.begin();
 
         try {
-            List<Member> result = em.createQuery("select m from Member m where m.username like '%kim%'", Member.class).getResultList();
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Member> query = cb.createQuery(Member.class);
 
-            for (Member member : result) {
-                System.out.println("member = " + member);
-            }
+            Root<Member> m = query.from(Member.class);
+
+            CriteriaQuery<Member> criteriaQuery = query.select(m).where(cb.equal(m.get("username"), "kim"));
+            // 물론 오타가 날 우려가 없다.
+            // 동적 쿼리고 깔끔
+            // 지금은 간단해서 쓸만해 보이지만 복잡한 쿼리에선 글쎄.. 실무에선 안쓴다...
+            List<Member> resultList = em.createQuery(criteriaQuery).getResultList();
 
             tx.commit();
         } catch (Exception e) {
