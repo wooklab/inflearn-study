5ea2c926c35519a34d3b63e0450f628c1acd3dfb
Hwang In-wook <inwook9@gmail.com>
2022-06-29 22:59:44 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편 값 타입: 실전 예제 6 - 값 타입 매핑

--- jpa-basic/src/main/java/jpabook/jpashop/domain/ShopMember.java
+++ jpa-basic/src/main/java/jpabook/jpashop/domain/ShopMember.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain;
 
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -21,11 +22,8 @@ public class ShopMember extends BasedEntity {
 
     private String name;
 
-    private String city;
-
-    private String street;
-
-    private String zipcode;
+    @Embedded
+    private ShopAddress address;
 
     @OneToMany(mappedBy = "member") // 관심사를 끊어내는 것이 중요한데 member 에 orders 가 필요하지 않지만 테스트를 위해 추가
     private List<Order> orders = new ArrayList<>(); // 관례상 초기화를 하자
@@ -49,27 +47,19 @@ public class ShopMember extends BasedEntity {
         this.name = name;
     }
 
-    public String getCity() {
-        return city;
+    public ShopAddress getAddress() {
+        return address;
     }
 
-    public void setCity(String city) {
-        this.city = city;
+    public void setAddress(ShopAddress address) {
+        this.address = address;
     }
 
-    public String getStreet() {
-        return street;
+    public List<Order> getOrders() {
+        return orders;
     }
 
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
     }
 }
