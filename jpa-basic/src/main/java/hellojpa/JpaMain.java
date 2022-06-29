a24f8ab110a5c70400c8614d0778b35f6735efbf
Hwang In-wook <inwook9@gmail.com>
2022-06-29 23:12:17 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편 객체지향 쿼리 언어1 - 기본문법: 소개 - 기본 JPQL 쿼리 생성확인

--- jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -16,37 +17,11 @@ public class JpaMain {
         tx.begin();
 
         try {
+            List<Member> result = em.createQuery("select m from Member m where m.username like '%kim%'", Member.class).getResultList();
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setHomeAddress(new Address("homeCity", "street", "10000"));
-
-            member.getFavoriteFood().add("치킨");
-            member.getFavoriteFood().add("족발");
-            member.getFavoriteFood().add("피자");
-
-            member.getAddressHistory().add(new AddressEntity("old1", "street", "10000"));
-            member.getAddressHistory().add(new AddressEntity("old2", "street", "10000"));
-
-            em.persist(member);
-
-            em.flush();
-            em.clear();
-
-            System.out.println("========= START =========");
-            Member findMember = em.find(Member.class, member.getId());
-
-            // homeCity -> newCity
-//            Address oldAddress = findMember.getHomeAddress();
-//            findMember.setHomeAddress(new Address("newCity", oldAddress.getStreet(), oldAddress.getZipcode()));
-
-            // 치킨 -> 한식
-//            findMember.getFavoriteFood().remove("치킨");
-//            findMember.getFavoriteFood().add("한식");
-
-            // remove() -> equals & hashcode 사용
-//            findMember.getAddressHistory().remove(new Address("old1", "street", "10000"));
-//            findMember.getAddressHistory().add(new Address("newCity1", "street", "10000"));
+            for (Member member : result) {
+                System.out.println("member = " + member);
+            }
 
             tx.commit();
         } catch (Exception e) {
