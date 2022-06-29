112941d280509f4681ff49cdada80d71ff420928
Hwang In-wook <inwook9@gmail.com>
2022-06-29 23:33:25 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편 객체지향 쿼리 언어1 - 기본문법: 소개 - native query 기본 작성

--- jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -4,9 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 import java.util.List;
 
 public class JpaMain {
@@ -20,16 +17,9 @@ public class JpaMain {
         tx.begin();
 
         try {
-            CriteriaBuilder cb = em.getCriteriaBuilder();
-            CriteriaQuery<Member> query = cb.createQuery(Member.class);
-
-            Root<Member> m = query.from(Member.class);
-
-            CriteriaQuery<Member> criteriaQuery = query.select(m).where(cb.equal(m.get("username"), "kim"));
-            // 물론 오타가 날 우려가 없다.
-            // 동적 쿼리고 깔끔
-            // 지금은 간단해서 쓸만해 보이지만 복잡한 쿼리에선 글쎄.. 실무에선 안쓴다...
-            List<Member> resultList = em.createQuery(criteriaQuery).getResultList();
+            // dynamic native SQL query 가 자동 생성
+            List<Member> resultList = em.createNativeQuery("select MEMBER_ID, city, street, zipcode, USERNAME from MEMBER", Member.class)
+                                        .getResultList();
 
             tx.commit();
         } catch (Exception e) {
