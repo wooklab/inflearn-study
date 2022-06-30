6f8acfb8b519a83256ad093b53abbb63bf71fd3e
Hwang In-wook <inwook9@gmail.com>
2022-06-30 23:52:07 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편 객체지향 쿼리 언어1 - 기본문법: 기본 문법과 쿼리 API - 프로젝션(SELECT): 임베디드 타입 프로젝션

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -26,14 +26,9 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            // m.team -> return Team
-            List<Team> result = em.createQuery("select m.team from Member m", Team.class)   // 자동 team join (묵시적 join)
-                                  .getResultList();
-
-            // 위와 동일한 결과
-            // 아래처럼 명시적으로 join이 발생됨을 알려주도록 하는 것을 권장
-            List<Team> result2 = em.createQuery("select t from Member m join m.team t", Team.class)   // 명시적 team join
-                                   .getResultList();
+            // 임베디드 타입 프로젝션은 엔티티를 통해서만 프로젝션 할 수 있다
+            List<Address> result = em.createQuery("select o.address from Order o", Address.class)
+                                     .getResultList();
 
             tx.commit();
         } catch (Exception e) {
