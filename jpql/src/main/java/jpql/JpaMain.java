e7c33f2fdd6cd079de25c95b746eebfff8b761eb
Hwang In-wook <inwook9@gmail.com>
2022-06-30 22:59:16 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편 객체지향 쿼리 언어1 - 기본문법: 기본 문법과 쿼리 API -TypeQuery & Query

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -4,6 +4,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 public class JpaMain {
 
@@ -20,6 +22,10 @@ public class JpaMain {
             Member member = new Member();
             member.setUsername("member1");
             em.persist(member);
+
+            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
+            Query query2 = em.createQuery("select m.username, m.age from Member m");    // 반환 타입이 다음과 같이 복수개 일 때
+            TypedQuery<String> query3 = em.createQuery("select m.username, m.age from Member m", String.class); // String으로 타입쿼리 가능
 
             tx.commit();
         } catch (Exception e) {
