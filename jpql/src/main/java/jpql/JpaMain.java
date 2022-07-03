331f32e31e55eea44e82ceab7fcfe600b2d03224
Hwang In-wook <inwook9@gmail.com>
2022-07-03 23:45:59 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편 객체지향 쿼리 언어2 - 중급 문법: 경로 표현식 - 경로 표현식 3가지

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.Collection;
 import java.util.List;
 
 public class JpaMain {
@@ -18,20 +19,31 @@ public class JpaMain {
 
         try {
 
+            Team team = new Team();
+            em.persist(team);
+
             Member member1 = new Member();
             member1.setUsername("관리자1");
+            member1.setTeam(team);
             em.persist(member1);
             Member member2 = new Member();
             member2.setUsername("관리자2");
+            member2.setTeam(team);
             em.persist(member2);
+
 
             em.flush();
             em.clear();
 
-            // group_concat 이미 있음, 사용자 정의 함수를 어떻게 사용하는지를 위해 별도 정의
-            String query = "select function('group_concat', m.username) from Member m";
-            List<String> result = em.createQuery(query, String.class).getResultList();
-            for (String s : result) {
+            /*String query = "select m.username from Member m";     // 1.상태 필드 (탐색 끝)
+            List<String> result = em.createQuery(query, String.class).getResultList();*/
+            /*String query = "select m.team from Member m";         // 2.단일 값 연관 필드
+            List<Team> result = em.createQuery(query, Team.class).getResultList();*/
+            String query = "select t.members from Team t";          // 3.컬렉션 값 연관 필드
+            Collection result = em.createQuery(query, Collection.class).getResultList();
+
+
+            for (Object s : result) {
                 System.out.println("s = " + s);
             }
             tx.commit();
