ae382ec157448eeaf86a392f64193388de1ede97
In-wook Hwang <inwook9@gmail.com>
2022-07-04 23:45:34 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편 객체지향 쿼리 언어2 - 중급 문법: 페치 조인 1 - 기본 - 3개의 Member 조회하기

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.Collection;
 import java.util.List;
 
 public class JpaMain {
@@ -19,33 +18,41 @@ public class JpaMain {
 
         try {
 
-            Team team = new Team();
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
 
             Member member1 = new Member();
-            member1.setUsername("관리자1");
-            member1.setTeam(team);
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
             em.persist(member1);
+
             Member member2 = new Member();
-            member2.setUsername("관리자2");
-            member2.setTeam(team);
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
             em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
 
             em.flush();
             em.clear();
 
-            /*String query = "select m.username from Member m";     // 1.상태 필드 (탐색 끝)
-            List<String> result = em.createQuery(query, String.class).getResultList();*/
-            /*String query = "select m.team from Member m";         // 2.단일 값 연관 필드
-            List<Team> result = em.createQuery(query, Team.class).getResultList();*/
-            String query = "select m.username from Team t join t.members m";          // 3.컬렉션 값 연관 필드(FROM 절에서 명시적 조인사용하여 필드 접근)
-            List<String> result = em.createQuery(query, String.class).getResultList();
+            String query = "select m from Member m";
+            List<Member> result = em.createQuery(query, Member.class)
+                    .getResultList();
 
-
-            for (String s : result) {
-                System.out.println("s = " + s);
+            for (Member member : result) {
+                System.out.println("member = " + member.getUsername() + ", " + member.getTeam().getName());
             }
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
