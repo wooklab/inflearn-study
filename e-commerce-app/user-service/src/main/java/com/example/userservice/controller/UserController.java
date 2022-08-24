38b11c43c82959a742de43d53aa38db685b8b93f
Hwang In-wook <inwook9@gmail.com>
2022-08-25 00:13:51 +0900

    e-commerce app
    application.yml 값 읽어오기

--- e-commerce-app/user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ e-commerce-app/user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -1,15 +1,28 @@
 package com.example.userservice.controller;
 
+import com.example.userservice.vo.Greeting;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
+@RequiredArgsConstructor
 public class UserController {
 
-    @GetMapping("health_check")
+    private final Environment env;
+    private final Greeting greeting;
+
+    @GetMapping("/health_check")
     public String status() {
         return "It's Working in User Service";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+//        return env.getProperty("greeting.message");
+        return greeting.getMessage();
     }
 }
