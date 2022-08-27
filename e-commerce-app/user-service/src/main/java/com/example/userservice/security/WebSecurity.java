6de269e45322082903bfb8718f25c221b4436dec
Hwang In-wook <inwook9@gmail.com>
2022-08-27 23:17:29 +0900

    add authenticationFilter on user-service

--- e-commerce-app/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ e-commerce-app/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -5,15 +5,31 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.servlet.Filter;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
+    private static final String LOCAL_IP_ADDRESS = "172.30.1.255";  // 본인 IP로 사용
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/user/**").permitAll();
+//        http.authorizeRequests().antMatchers("/user/**").permitAll();
+        http.authorizeRequests()
+            .antMatchers("/**")
+            .hasIpAddress(LOCAL_IP_ADDRESS)
+            .and()
+            .addFilter(getAuthenticationFilter());
 
         http.headers().frameOptions().disable();    // for h2-console access
+    }
+
+    private AuthenticationFilter getAuthenticationFilter() throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        authenticationFilter.setAuthenticationManager(authenticationManager());
+
+        return authenticationFilter;
     }
 }
