b92aec6cdbf1f2a4b92d2daa76e1c01289e1c4cf
Hwang In-wook <inwook9@gmail.com>
2022-08-27 22:18:48 +0900

    order-service implements

---
+++ e-commerce-app/order-service/src/main/java/com/example/orderservice/vo/ResponseOrder.java
@@ -0,0 +1,17 @@
+package com.example.orderservice.vo;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ResponseOrder {
+    private String productId;
+    private Integer qty;
+    private Integer unitPrice;
+    private Integer totalPrice;
+    private LocalDate createdAt;
+    private String orderId;
+}
