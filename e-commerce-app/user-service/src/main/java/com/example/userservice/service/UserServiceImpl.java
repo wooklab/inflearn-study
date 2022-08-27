75dc9014f741b5c796394637445c21484cde4332
Hwang In-wook <inwook9@gmail.com>
2022-08-27 23:03:47 +0900

    user-service 에 ModelMapper Bean으로 변경

--- e-commerce-app/user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ e-commerce-app/user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -6,7 +6,6 @@ import com.example.userservice.jpa.UserRepository;
 import com.example.userservice.vo.ResponseOrder;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -22,19 +21,18 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder passwordEncoder;
+    private final ModelMapper modelMapper;
 
     @Override
     public UserDto createUser(UserDto userDto) {
         userDto.setUserId(UUID.randomUUID().toString());
 
-        ModelMapper mapper = new ModelMapper();
-        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
-        UserEntity userEntity = mapper.map(userDto, UserEntity.class);
+        UserEntity userEntity = modelMapper.map(userDto, UserEntity.class);
         userEntity.setEncryptedPwd(passwordEncoder.encode(userDto.getPwd()));
 
         userRepository.save(userEntity);
 
-        return mapper.map(userEntity, UserDto.class);
+        return modelMapper.map(userEntity, UserDto.class);
     }
 
     @Override
@@ -45,7 +43,7 @@ public class UserServiceImpl implements UserService {
             throw new UsernameNotFoundException("User not found");
         }
 
-        UserDto userDto = new ModelMapper().map(userEntity, UserDto.class);
+        UserDto userDto = modelMapper.map(userEntity, UserDto.class);
 
         List<ResponseOrder> orders = new ArrayList<>();
         userDto.setOrders(orders);
