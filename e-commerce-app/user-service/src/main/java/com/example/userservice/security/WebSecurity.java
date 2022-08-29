d773accf78fed9d9eacd55290ba62066bc89b551
Hwang In-wook <inwook9@gmail.com>
2022-08-29 22:51:06 +0900

    user-service: loadUserByUsername() 구현

--- e-commerce-app/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ e-commerce-app/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -1,17 +1,25 @@
 package com.example.userservice.security;
 
+import com.example.userservice.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-import javax.servlet.Filter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     private static final String LOCAL_IP_ADDRESS = "172.30.1.255";  // 본인 IP로 사용
+
+    private final UserService userService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final Environment env;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -31,5 +39,12 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         authenticationFilter.setAuthenticationManager(authenticationManager());
 
         return authenticationFilter;
+    }
+
+    // select pwd from users where email = ?
+    // db_pwd(encrypted) == input_pwd(encrypted)
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
     }
 }
--- e-commerce-app/user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ e-commerce-app/user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -6,6 +6,8 @@ import com.example.userservice.jpa.UserRepository;
 import com.example.userservice.vo.ResponseOrder;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -22,6 +24,21 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder passwordEncoder;
     private final ModelMapper modelMapper;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity userEntity = userRepository.findByEmail(username);
+        if (userEntity == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        return new User(userEntity.getEmail(),
+                        userEntity.getEncryptedPwd(),
+                        true,
+                        true,
+                        true,
+                        true,
+                        new ArrayList<>());
+    }
 
     @Override
     public UserDto createUser(UserDto userDto) {
