d5f047df6df62875e1ee43a3ae26b5a3f78c2787
Hwang In-wook <inwook9@gmail.com>
2022-09-02 00:25:30 +0900

    gateway에 actuator를 사용할수 있도록 설정

--- spring-cloud-msa/api-gateway-service/src/main/java/com/example/apigatewayservice/ApiGatewayServiceApplication.java
+++ spring-cloud-msa/api-gateway-service/src/main/java/com/example/apigatewayservice/ApiGatewayServiceApplication.java
@@ -1,7 +1,10 @@
 package com.example.apigatewayservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiGatewayServiceApplication {
@@ -10,4 +13,8 @@ public class ApiGatewayServiceApplication {
         SpringApplication.run(ApiGatewayServiceApplication.class, args);
     }
 
+    @Bean
+    public HttpTraceRepository httpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
+    }
 }
