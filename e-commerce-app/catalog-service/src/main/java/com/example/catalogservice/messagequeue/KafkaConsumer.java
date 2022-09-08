04bf3db07ee74218985e1815960bf664138e8cd9
Hwang In-wook <inwook9@gmail.com>
2022-09-09 00:10:02 +0900

    catalog-service에 Kafka 구현

---
+++ e-commerce-app/catalog-service/src/main/java/com/example/catalogservice/messagequeue/KafkaConsumer.java
@@ -0,0 +1,44 @@
+package com.example.catalogservice.messagequeue;
+
+import com.example.catalogservice.jpa.CatalogEntity;
+import com.example.catalogservice.jpa.CatalogRepository;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class KafkaConsumer {
+
+    private final CatalogRepository catalogRepository;
+
+    @KafkaListener(topics = "example-catalog-topic")
+    public void updateQty(String kafkaMessage) {
+        log.info("Kafka Message -> {}", kafkaMessage);
+
+        Map<Object, Object> map = new HashMap<>();
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            map = objectMapper.readValue(kafkaMessage, new TypeReference<>() {
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        String productId = (String) map.get("productId");
+        Integer qty = (Integer) map.get("qty");
+        CatalogEntity catalogEntity = catalogRepository.findByProductId(productId);
+        if (catalogEntity != null) {
+            catalogEntity.setStock(catalogEntity.getStock() - qty);
+            catalogRepository.save(catalogEntity);
+        }
+    }
+}
